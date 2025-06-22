a74678142d9e4a40a1f84963f3c3070e17147d12
Nancy Piedra <nancy_piedra@yahoo.com>
2025-06-22 08:53:17 -0400

    add debug statements to route.ts

--- app/api/list-users/route.ts
+++ app/api/list-users/route.ts
@@ -7,7 +7,11 @@ import {
 
 export async function GET(request: Request) {
   const authHeader = request.headers.get('authorization'); // e.g., "Bearer eyJ..."
+  console.log("authHeader");
+  console.log(authHeader);
   const token = authHeader?.split(' ')[1]; // Extract token from "Bearer <token>"
+  console.log("token:");
+  console.log(token);
 
   if (!token) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
@@ -16,19 +20,26 @@ export async function GET(request: Request) {
   const lambdaClient = new LambdaClient({ region: 'us-east-1' });
 
   const payload = JSON.stringify({ token });
+  console.log("payload:");
+  console.log(payload);
 
   const command = new InvokeCommand({
     FunctionName: 'listUsersFunction-staging',
     InvocationType: 'RequestResponse',
     Payload: new TextEncoder().encode(payload),
   });
+  console.log("command:");
+  console.log(command);
 
   try {
     const response = await lambdaClient.send(command);
-
+    console.log("response:");
+    console.log(response);
     const payload = JSON.parse(
       new TextDecoder('utf-8').decode(response.Payload)
     );
+    console.log("payload:");
+    console.log(payload);
 
     return NextResponse.json(payload);
   } catch (error) {
